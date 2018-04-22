ccbccc7a33caab08d2667a1967330d3746c901a2
blsss <35145282+bellesse@users.noreply.github.com>
2018-04-22 18:34:22 +0300

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,23 @@
+import discord
+import asyncio
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+    print('Logged in as')
+    print(client.user.name)
+    print(client.user.id)
+    print('------')
+
+@client.event
+async def on_message(message):
+    if message.content.startswith('~123test'):
+        counter = 0
+        tmp = await client.send_message(message.channel, 'Calculating messages...')
+        async for log in client.logs_from(message.channel, limit=100):
+            if log.author == message.author:
+                counter += 1
+
+	
+client.run('NDM3MzU5NTQwODU4NzgxNjk3.Db4y3A.0B123f7HlyYmFl3Pt3WPL8cNM2w')
